21dbd5b9198efbf53a4dff515d5cb104659fbd85
Igor Petrov <Igor_Petrov@epam.com>
2018-09-11 13:12:40 +0400

    extract email from userinfo and/or id_token

--- src/main/java/ru/coddebattle/oidc/conf/SecurityConfig.java
+++ src/main/java/ru/coddebattle/oidc/conf/SecurityConfig.java
@@ -7,6 +7,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Map;
 
 @Configuration
 @Order(1)
@@ -21,7 +30,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin().permitAll()
             .and()
-            .oauth2Login().permitAll();
+            .oauth2Login()
+                .permitAll()
+                .userInfoEndpoint().userService(new DefaultOAuth2UserService() {
+            @Override
+            public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+                OAuth2User oauthUser = super.loadUser(userRequest);
+                Map<String, Object> userAttributes = oauthUser.getAttributes();
+                // github
+                String userName = (String) userAttributes.get("login");
+                String avatarUri = (String) userAttributes.get("avatar_url");
+                String email = (String) userAttributes.get("email");
+                return oauthUser;
+            }
+        })
+        .oidcUserService(new OidcUserService() {
+            @Override
+            public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+                OidcUser oidcUser = super.loadUser(userRequest);
+                Map<String, Object> attributes = oidcUser.getAttributes();
+                String email = (String) attributes.get("email");
+                String givenName = (String) attributes.get("given_name");
+                return oidcUser;
+            }
+        })
+        ;
     } // @formatter:on
 
     @Override
